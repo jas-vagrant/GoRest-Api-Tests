07cb4df55f725e471003b2d2fc9a6ff8c3772759
Jaspreet Singh <jaspreetsingh@Jaspreets-MacBook-Pro.local>
2022-01-18 14:17:44 +0530

    create user test added

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,5 +14,25 @@ public class SampleTest {
                     .get("https://gorest.co.in/public/v1/users")
                 .then()
                     .statusCode(200).log().body();
+    }
+
+    @Test
+    public void shouldCreateUser(){
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 174e2d0a979662dd305a3d6aaee9dc8f16874404c4a120bd15ad22efa95eb0e6")
+                .body("{\n" +
+                        "  \"name\": \"Jaspreet Singh\",\n" +
+                        "  \"gender\": \"male\",\n" +
+                        "  \"email\": \"tenali.jsingh@153ce.com\",\n" +
+                        "  \"status\": \"active\"\n" +
+                        "}")
+        .when()
+                .post("https://gorest.co.in/public/v1/users")
+        .then()
+                .log().body()
+                .statusCode(201);
     }
 }
